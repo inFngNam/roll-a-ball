ce3d5ba41f9bab81225f718ba8c63f7be083eaa7
inFngNam <inf.ng.nam@gmail.com>
2022-09-12 00:35:24 +0700

    Final

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,21 +1,50 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] public float movementSpeed;
+    [SerializeField] public TextMeshProUGUI countText;
+    [SerializeField] public GameObject winTextObject;
 
+    private Rigidbody rb;
     private float movementX;
     private float movementZ;
 
-    private Rigidbody rb;
-
+    private int count = 0;
+    private float cooldown = 5f;
+    private bool isFinished = false;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        SetCountText();
+        winTextObject.SetActive(false);
+    }
+
+    private void SetCountText()
+    {
+        countText.text = "Count: " + count;
+        if (count >= 8)
+        {
+            winTextObject.SetActive(true);
+            isFinished = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (isFinished)
+        {
+            cooldown -= Time.deltaTime;
+
+            if (cooldown <= 0)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
     }
 
     private void OnMove(InputValue movementValue)
@@ -29,5 +58,15 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 movement = new Vector3(movementX, 0, movementZ);
         rb.AddForce(movement * movementSpeed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Roll A Ball
+
 ![Alt text](Image/Introduce.png)
 [Course](https://learn.unity.com/project/roll-a-ball?uv=2019.4)
